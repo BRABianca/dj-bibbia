fed99cbcd491ab8cd38cbc34327ff649046c06d9
BRABianca <avalos.bianca@ufms.br>
2025-03-27 22:43:10 -0400

    fix; embed youtube

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,34 @@
-const personagens = document.querySelectorAll(".personagem")
-const botoes = document.querySelectorAll(".botao")
+const personagens = document.querySelectorAll(".personagem");
+const botoes = document.querySelectorAll(".botao");
 
 botoes.forEach((botao, i) => {
-    botao.addEventListener("click", () =>{
-        removerSelecaoBotao();      
-        botao.classList.add("selecionado")
+  botao.addEventListener("click", () => {
+    removerSelecaoBotao();
+    botao.classList.add("selecionado");
 
-        removerSelecaoPersonagem();
-        personagens[i].classList.add("selecionado")
-    })
+    removerSelecaoPersonagem();
+    personagens[i].classList.add("selecionado");
+
+    // Lazy load do iframe (YouTube, SoundCloud, etc)
+    const personagemAtivo = personagens[i];
+    const iframe = personagemAtivo.querySelector("iframe");
+
+    if (iframe && iframe.dataset.src && !iframe.src) {
+      iframe.src = iframe.dataset.src;
+    }
+  });
 });
 
 function removerSelecaoBotao() {
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  if (botaoSelecionado) {
     botaoSelecionado.classList.remove("selecionado");
+  }
 }
 
 function removerSelecaoPersonagem() {
-    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+  const personagemSelecionado = document.querySelector(".personagem.selecionado");
+  if (personagemSelecionado) {
     personagemSelecionado.classList.remove("selecionado");
+  }
 }
